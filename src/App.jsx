170fd529d268462ef30306cac606e1261fe03b9d
German <fedosihingerman@yandex.ru>
2025-06-01 15:27:52 +0300

    wtf with Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import { useState, useEffect } from "react";
 import ProductModal from "./ProductModal";
-import Cart from "./Cart";
+import Cart from "./Cart.jsx";
 import SymptomModal from "./SymptomModal";
 
 const loadFromLocalStorage = () => {
